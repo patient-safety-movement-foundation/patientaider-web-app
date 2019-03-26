7bd16f2b1d7c15c3d81ead2ba20005b79bb1ed11
ewolfe <e@ewolfe.me>
2019-03-25 23:49:09 -0700

    fix: contentful markup

--- src/components/layout.js
+++ src/components/layout.js
@@ -158,12 +158,12 @@ const Layout = ({ children, location }) => (
           <p>
             <a
               href="https://www.contentful.com/"
-              rel="nofollow"
+              rel="noopener noreferrer"
               target="_blank"
             >
               <img
                 src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
-                style="max-width:100px;width:100%;"
+                style={{ maxWidth: '100px', width: '100%' }}
                 alt="Powered by Contentful"
               />
             </a>
