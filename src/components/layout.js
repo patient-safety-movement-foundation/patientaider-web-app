74889cff1c912de20850a3e75b207906c274a356
ewolfe <e@ewolfe.me>
2019-03-25 23:27:42 -0700

    Add "Powered By Contentful" logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -156,6 +156,11 @@ const Layout = ({ children, location }) => (
             />
           </p>
           <p>
+            <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
+              <img src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" style="max-width:100px;width:100%;" alt="Powered by Contentful" />
+             </a>
+          </p>
+          <p>
             <Link
               to={`${location.pathname}?${qs.stringify({
                 ...qs.parse(location.search, {
